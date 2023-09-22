626e1e14725cac3a32842487e5cc62df66fc04c3
nirogu <nirogu@outlook.com>
2023-09-21 23:48:39 -0500

    Remove lua community setup
    Necessary due to bug with mason and luacheck

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,7 +2,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.json" },
